58dd4ec560aef9d79cfed6baaec087b37b75e1e8
Antoine Landrieux <101741720+AntoineLandrieux@users.noreply.github.com>
2022-10-21 21:53:45 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -18,23 +18,3 @@ window.addEventListener('scroll', function (f) {
         logo.style.transform = "translate(0, 0)";
     }
 });
-
-const suggestions = [
-    {name: "Information", redirect: "whatisit"},
-    {name: "Télécharger", redirect: "download"},
-    {name: "Main Menu",   redirect: "main"},
-    {name: "License",     redirect: "ref"}
-];
-
-const searchInput = document.getElementById("search");
-const suggestion = document.getElementById("sugg");
-
-searchInput.addEventListener('keyup', function (f) {
-    let sug = '';
-    input = searchInput.value;
-    if (input != '') {
-      const result = suggestions.filter(Item => Item.name.toLowerCase().includes(input.toLowerCase()));
-      result.forEach(resultItem => sug += `<a href="#${resultItem.redirect}">${resultItem.name}</a>`);    
-    }
-    suggestion.innerHTML = sug;
-});

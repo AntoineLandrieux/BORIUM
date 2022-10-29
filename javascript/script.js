05ee1dce50b64bc7d7c932bae46271cbf04b3ebb
Antoine Landrieux <101741720+AntoineLandrieux@users.noreply.github.com>
2022-10-29 12:14:56 +0200

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -2,6 +2,13 @@ const nav = document.querySelector("nav");
 const btn = document.querySelector("button");
 const logo = document.querySelector("img");
 
+window.addEventListener('load', function (event)
+{
+    if (event.isTrusted == true) {
+        document.getElementById("load").style.visibility = "hidden";
+    }
+});
+
 btn.addEventListener('click', function (f) {
     nav.classList.toggle("enable");
     nav.classList.toggle("disable");
